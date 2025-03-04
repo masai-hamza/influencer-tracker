55a634d58ac632974f11dbfe26b0c8ab67fc822c
Hamza <inno@Innos-MacBook-Pro.local>
2025-03-04 17:01:09 +0530

    Updated backend for PostgreSQL and deployment setup

--- app/__init__.py
+++ app/__init__.py
@@ -1,16 +1,20 @@
 from flask import Flask  
 from flask_sqlalchemy import SQLAlchemy  
-from flask_migrate import Migrate  
+from config import Config  
+from dotenv import load_dotenv  
+import os  
 
+load_dotenv()  # Load environment variables from .env  
 db = SQLAlchemy()  
-migrate = Migrate()  
 
 def create_app():  
     app = Flask(__name__)  
-    app.config.from_object("config.Config")  
-    
+    app.config.from_object(Config)  
+
     db.init_app(app)  
-    migrate.init_app(app, db)  
+
+    with app.app_context():  
+        db.create_all()  # Create tables if they don't exist  
 
     from app.routes import main  
     app.register_blueprint(main)  
--- config.py
+++ config.py
@@ -1,6 +1,9 @@
+# config.py  
 import os  
 
-class Config:  
-    SECRET_KEY = "your-secret-key"  
-    SQLALCHEMY_DATABASE_URI = "sqlite:///app.db"  
-    SQLALCHEMY_TRACK_MODIFICATIONS = False  
+class Config: 
+    SECRET_KEY = os.getenv("SECRET_KEY", "09aaa368aada176812c0443f275c915e") 
+    SQLALCHEMY_DATABASE_URI = os.getenv(  
+        "DATABASE_URL","postgresql://postgres:c29O5VLtglNppCqlVXus@analyticsdb.crfg8xlnzwpc.ap-south-1.rds.amazonaws.com:5432/analytics-db"
+    )  
+    SQLALCHEMY_TRACK_MODIFICATIONS = False  # Disable event system for performance  
